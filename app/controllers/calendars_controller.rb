69e2dc49b45ae44b9d399fc0e7e36faa6cd921af
miyazakisayaka7 <une_charrue@yahoo.co.jp>
2021-04-01 19:06:38 +0900

    issue4~6修正②

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    get_week
+    getWeek
     @plan = Plan.new
   end
 
@@ -39,7 +39,7 @@ class CalendarsController < ApplicationController
       if wday_num >= 7
         wday_num = wday_num -7
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => (@todays_date + x) + 1}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: (@todays_date + x) + 1}
     end
   end
-  wdays[]
+end
